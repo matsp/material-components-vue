1142e56a5d46656949c8ff211501b6b8a6073a5e
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2017-10-02 15:47:45 +0200

    Update Textfield.vue

--- src/components/Textfield/Textfield.vue
+++ src/components/Textfield/Textfield.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mdc-textfield" :class="classes">
         <i v-if="leadingIcon" class="material-icons mdc-textfield__icon">{{leadingIcon}}</i>
-        <input :id="id" :class="mdc-textfield__input" :value="value" @input="updateModel" :placeholder="placeholder" :type="type" v-bind="$attrs" />
+        <input :id="id" class="mdc-textfield__input" :value="value" @input="updateModel" :placeholder="placeholder" v-bind="$attrs" />
         <label v-if="label" class="mdc-textfield__label" :class="classesLabel">{{label}}</label>
         <i v-if="trailingIcon" class="material-icons mdc-textfield__icon">{{trailingIcon}}</i>
         <div v-if="bottomLine" class="mdc-textfield__bottom-line"></div>
@@ -18,10 +18,6 @@ export default {
         label: {
             type: String,
             required: false
-        },
-        type: {
-            type: String,
-            default: 'text'
         },
         value: {
             type: String,
