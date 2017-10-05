5d44b4b0adfbe91cfffcce693c349930b9f3390c
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2017-10-05 07:29:13 +0200

    Update Snackbar.vue

--- src/components/Snackbar/Snackbar.vue
+++ src/components/Snackbar/Snackbar.vue
@@ -33,7 +33,7 @@ export default {
             required: true
         },
         timeout: {
-            type: Integer,
+            type: Number,
             required: false
         },
         actionHandler: {
