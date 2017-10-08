03cac4bc472a98e5a5b401b96601feaa4d5494ab
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2017-10-08 13:53:23 +0200

    Finalized switch component.

--- src/Switch/Switch.vue
+++ src/Switch/Switch.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="mdc-switch" :class="classes">
-        <input v-if="disabled" type="checkbox" class="mdc-switch__native-control" disabled/>
-        <input v-else type="checkbox" class="mdc-switch__native-control" />
+        <input :id="id" v-model="model" type="checkbox" class="mdc-switch__native-control" :disabled="disabled" />
         <div class="mdc-switch__background">
             <div class="mdc-switch__knob"></div>
         </div>
@@ -15,15 +14,31 @@ export default {
             type: String,
             required: false
         },
+        checked: {
+            type: Boolean,
+            required: true
+        },
         disabled: {
             type: Boolean,
             required: false
         }
     },
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
     computed: {
         classes() {
             return {
                 'mdc-switch--disabled': this.disabled
+            }
+        },
+        model: {
+            get() {
+                return this.checked
+            },
+            set(state) {
+                this.$emit('change', state)
             }
         }
     }
