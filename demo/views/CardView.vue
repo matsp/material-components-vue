c28ccd453c5919bdadab682b0394aea2f9042fd4
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2017-10-21 12:38:22 +0200

    Extended card demo.

--- demo/views/CardView.vue
+++ demo/views/CardView.vue
@@ -1,10 +1,21 @@
 <template>
     <m-layout-grid-inner>
+        <m-layout-grid-cell :span="12">
+            <m-typo-display :level="1">Card</m-typo-display>
+                        <m-typo-body :level="2">
+                <a href="https://github.com/matsp/material-components-vue/blob/master/components/Card">
+                    https://github.com/matsp/material-components-vue/blob/master/components/Card
+                </a>
+            </m-typo-body>
+         </m-layout-grid-cell>
         <m-layout-grid-cell>
             <m-card>
                 <m-typo-title slot="title">Title</m-typo-title>
                 <m-typo-subheading :level="1" slot="subtitle">Subtitle</m-typo-subheading>
                 <m-typo-body :level="1" slot="supportingText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</m-typo-body>
+                <m-button interactive slot="actions" href="https://github.com/matsp/material-components-vue/blob/master/components/Card">
+                    github
+                </m-button>
             </m-card>
         </m-layout-grid-cell>
     </m-layout-grid-inner>
