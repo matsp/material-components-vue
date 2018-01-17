68980b76ca55f8ea51a4d578bc1d1e1dae3cf9d7
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-01-17 19:46:32 +0100

    refactor(demo): Externalize styles

--- demo/App.vue
+++ demo/App.vue
@@ -3,21 +3,36 @@
   :dark="darkTheme">
     <m-typography class="demo-body">
 
-      <m-toolbar ref="toolbar" fixed waterfall>
+      <m-toolbar
+        ref="toolbar"
+        fixed
+        waterfall>
         <m-toolbar-row shrink-center>
-          <m-toolbar-icon slot="start" icon="menu" menu-icon @click="toggleDrawer" />
+          <m-toolbar-icon
+            slot="start"
+            icon="menu"
+            menu-icon
+            @click="toggleDrawer"/>
           Demo
-          <m-form-field align-end slot="end" class="demo-toolbar-row-right">
+          <m-form-field
+            align-end
+            slot="end"
+            class="demo-toolbar-row-right">
             <m-switch @change="toggleDarkTheme()"/>
             <label>Dark theme</label>
           </m-form-field>
         </m-toolbar-row>
       </m-toolbar>
 
-      <m-temporary-drawer ref="drawer" @click="toggleDrawer">
+      <m-temporary-drawer
+        ref="drawer"
+        @click="toggleDrawer">
         <span slot="toolbarSpacer" />
         <m-list>
-          <m-list-item v-for="item in listItems" :key="item.text" @click="openRoute(item.route)">
+          <m-list-item
+            v-for="item in listItems"
+            :key="item.text"
+            @click="openRoute(item.route)">
             <m-icon
               slot="graphic"
               :icon="item.icon"/>
@@ -59,6 +74,14 @@ export default {
       }
     }
   },
+  computed: {
+    listItems () {
+      return this.$store.state.app.drawerListItems
+    },
+    darkTheme () {
+      return this.$store.state.app.darkTheme
+    }
+  },
   methods: {
     toggleDrawer () {
       this.$refs.drawer.toggle()
@@ -69,55 +92,10 @@ export default {
     toggleDarkTheme () {
       this.$store.dispatch('toggleDarkTheme')
     }
-  },
-  computed: {
-    listItems () {
-      return this.$store.state.app.drawerListItems
-    },
-    darkTheme () {
-      return this.$store.state.app.darkTheme
-    }
   }
 }
 </script>
 
 <style lang="scss">
-$mdc-theme-primary: #5e35b1;
-$mdc-theme-secondary: #ff5722;
-$mdc-theme-background: #ffffff;
-
-@import "../components/Theme/theme.scss";
-@import "~normalize.css/normalize.css";
-// @import url('https://cdnjs.com/libraries/normalize')
-@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500");
-@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
-
-.demo-body {
-  display: flex;
-  flex-direction: row;
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  height: 100%;
-  width: 100%;
-}
-
-.demo-content {
-  margin: 0;
-  display: inline-flex;
-  flex-direction: column;
-  flex-grow: 1;
-  height: 100%;
-  box-sizing: border-box;
-}
-
-.demo-toolbar-row-right {
-  margin-left: 5px;
-  margin-right: 5px;
-}
-
-.mdc-theme--dark {
-  background-color: #303030;
-  color: #ffffff;
-}
+@import "styles/demo";
 </style>
