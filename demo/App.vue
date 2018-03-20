2bd93a2351419120bcd044787d55331032e12963
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-03-20 13:43:42 +0100

    Temporary changes

--- demo/App.vue
+++ demo/App.vue
@@ -98,8 +98,70 @@ $mdc-theme-background: #ffffff;
 @import "../dist/toolbar/styles";
 @import "../dist/typography/styles";
 @import "~normalize.css/normalize.css";
-@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500");
-@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+/* @import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500"); */
+/* @import url("https://fonts.googleapis.com/icon?family=Material+Icons"); */
+
+/* roboto-300 - latin */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 300;
+  src: local('Roboto Light'), local('Roboto-Light'),
+       url('./assets/fonts/roboto-300.woff2') format('woff2'),
+       url('./assets/fonts/roboto-300.woff') format('woff');
+}
+/* roboto-regular - latin */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Roboto'), local('Roboto-Regular'),
+       url('./assets/fonts/roboto-regular.woff2') format('woff2'),
+       url('./assets/fonts/roboto-regular.woff') format('woff');
+}
+/* roboto-500 - latin */
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  src: local('Roboto Medium'), local('Roboto-Medium'),
+       url('./assets/fonts/roboto-500.woff2') format('woff2'),
+       url('./assets/fonts/roboto-500.woff') format('woff');
+     }
+
+@font-face {
+  font-family: 'Material Icons';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Material Icons'), local('MaterialIcons-Regular'),
+       url('./assets/fonts/materialicons-regular.woff2') format('woff2'),
+       url('./assets/fonts/materialicons-regular.woff') format('woff');
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;  /* Preferred icon size */
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+}
 
 .demo-body {
   display: flex;
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -3,13 +3,16 @@ const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// const PreloadWebpackPlugin = require('preload-webpack-plugin')
+const PreloadWebpackPlugin = require('preload-webpack-plugin')
 
 const root = path.join(__dirname)
 const demo = path.join(root + '/demo/')
 const nodeModules = path.join(root, '/node_modules/')
 
 module.exports = {
+  // output: {
+    // publicPath: path.resolve(root + '/public')
+  // },
   entry: {
     app: [path.resolve(demo + 'index.js')]
   },
@@ -35,7 +38,7 @@ module.exports = {
                 loader: 'css-loader',
                 options: {
                   sourceMap: false,
-                  url: false,
+                  // url: false,
                   importLoaders: 1
                 }
               },
@@ -92,10 +95,21 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[chunkhash].min.css',
       chunkFilename: 'chunk.[chunkhash].min.css'
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'prefetch',
+      include: 'asyncChunks'
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'preload',
+      include: ['app']
+    }),
+    new PreloadWebpackPlugin({
+      rel: 'preload',
+      include: 'allAssets',
+      fileWhitelist: [/\.woff/, /\.woff2/],
+      fileBlacklist: [/\.js/, /\.css/] // only ie11+ needs woff
     })
-    // new PreloadWebpackPlugin({
-    // fileWhitelist: [/\.async\./],
-    // })
   ],
   stats: {
     all: false,
