905540c19683bf6ce2ceb33142f4760a38b3d691
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-03-23 23:06:33 +0100

    refactor(notched-outline): Delete not needed JS component import

--- components/notched-outline/NotchedOutline.vue
+++ components/notched-outline/NotchedOutline.vue
@@ -1,19 +1,9 @@
 <script>
-import { MDCNotchedOutline } from '@material/notched-outline'
-
 import themeClassMixin from '../base/themeClassMixin.js'
 
 export default {
   functional: true,
   mixins: [themeClassMixin],
-  data () {
-    return {
-      mdcNotchedOutline: null
-    }
-  },
-  mounted () {
-    this.mdcNotchedOutline = MDCNotchedOutline.attachTo(this.$el)
-  },
   render (h) {
     return [
       h('div', {'class': 'mdc-notched-outline'},
