af17558eef0fd9c64737886c4311d33733013999
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-03-25 22:15:22 +0200

    feat(chip): Synchronise with mdc-web v0.33.0 (add checkmark)

--- components/chips/Chip.vue
+++ components/chips/Chip.vue
@@ -5,6 +5,14 @@
     <slot
       name="leadingIcon"
       v-if="$slots['leadingIcon']"/>
+    <div class="mdc-chip__checkmark" >
+      <svg class="mdc-chip__checkmark-svg" viewBox="-2 -3 30 30">
+        <path
+          class="mdc-chip__checkmark-path"
+          fill="none" stroke="black"
+          d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+      </svg>
+    </div>
     <div class="mdc-chip__text">
       <slot/>
     </div>
