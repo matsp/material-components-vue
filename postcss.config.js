d985b59b35a9ad27a848731d334957dcf5ae74b1
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-04-06 11:24:21 +0200

    chore(postcss): Reactivate autoprefixer with additions

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,12 @@ module.exports = {
   plugins: {
     // 'postcss-import': {},
     // 'postcss-cssnext': {},
-    // 'cssnano': {
-    // 'autoprefixer': {
-    // 'add': true
-    // }
-    // }
+    'cssnano': {
+      'autoprefixer': {
+        'add': true
+      }
+    }
     // 'postcss-preset-env': {},
-    'cssnano': {}
+    // 'cssnano': {}
   }
 }
