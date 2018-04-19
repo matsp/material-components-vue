adea7db035ec7cb2e286f9da1c29419548903e71
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-04-19 11:12:18 +0200

    chore: Add vuepress to project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Material Components Vue',
+  description: 'Material Design styled components for Vue.js',
+  markdown: {
+    toc: {
+      includeLevel: [1, 2]
+    }
+  },
+  themeConfig: {
+    sidebar: [
+      '/Button'
+    ]
+  }
+}
---
+++ docs/Button.md
@@ -0,0 +1,3 @@
+# Button
+
+<ButtonDemo></ButtonDemo>
