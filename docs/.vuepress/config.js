4b65ad3210a197c30f1449b268ba22d4e7c85ce3
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-05-20 16:20:52 +0200

    docs: Use components directly (close #98)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,6 @@ const nodeModules = path.resolve('node_modules/')
 module.exports = {
   title: 'Material Components Vue',
   description: 'Material Design components for Vue.js',
-  base: '/material-components-vue/',
   head: [
     ['link', { rel: 'icon', href: '/logo.png'}]
   ],
