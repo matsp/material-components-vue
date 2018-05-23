f9802703d3759d09c740770f6c8a2431dce1cea9
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-05-23 11:47:37 +0200

    fix(list): Correct component names for instantiation (close #111)

--- components/list/index.js
+++ components/list/index.js
@@ -13,8 +13,8 @@ const plugin = {
     vm.component('m-list', List)
     vm.component('m-list-divider', ListDivider)
     vm.component('m-list-group', ListGroup)
-    vm.component('m-list-list-group-divider', ListGroupDivider)
-    vm.component('m-list-list-group-subheader', ListGroupSubheader)
+    vm.component('m-list-group-divider', ListGroupDivider)
+    vm.component('m-list-group-subheader', ListGroupSubheader)
     vm.component('m-list-item', ListItem)
   }
 }
