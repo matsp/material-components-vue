48b81ca31b50b5ab1e2b443e084349c9323375a5
jjchen1 <tychenjiajun@live.cn>
2019-07-26 16:13:26 +0800

    feat: update v-ripple directive
    bind `mdcRipple` to the el which makes it available for user to activate or deactivate using binding value on native html element

--- docs/.vuepress/components/RippleDemo.vue
+++ docs/.vuepress/components/RippleDemo.vue
@@ -2,7 +2,8 @@
     <div>
         <ComponentSection flex-direction="column">
             <my-button v-if="checkboxProps[0].value" v-ripple.primary="radioProps[0].value"/>
-            <my-button v-if="checkboxProps[1].value" v-ripple.accent="radioProps[0].value"/>
+            <button style="width: 100px; height: 40px; background: deepskyblue; outline: none; border: none; color: white; border-radius: 4px;" v-if="checkboxProps[1].value" v-ripple.accent="radioProps[0].value">BUTTON</button>
+            <my-button v-ripple.primary.css-only v-if="checkboxProps[2].value"/>
         </ComponentSection>
         <ComponentProps
                 :checkboxProps="checkboxProps"
@@ -23,7 +24,8 @@
         ],
         checkboxProps: [
           { prop: 'primary', value: false },
-          { prop: 'accent', value: true }
+          { prop: 'accent', value: true },
+          { prop: 'css-only', value: false}
         ]
       }
     }
