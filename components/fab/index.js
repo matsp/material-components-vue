ea2697b763e20df3c5b7fe478035ce5107df0e23
Jiajun Chen <tychenjiajun@live.cn>
2019-08-27 16:03:12 +0800

    feat(fab): add trailingIcon slot (#411)
    * feat(fab): add trailingIcon slot
    
    * docs(fab): update demo

--- components/fab/index.js
+++ components/fab/index.js
@@ -1,6 +1,4 @@
 import Fab from './Fab.vue'
-import FabLabel from './FabLabel'
-import FabIcon from './FabIcon'
 import './styles.scss'
 
 import { initPlugin } from '../'
@@ -8,8 +6,6 @@ import { initPlugin } from '../'
 const plugin = {
   install (vm) {
     vm.component('m-fab', Fab)
-    vm.component('m-fab-label', FabLabel)
-    vm.component('m-fab-icon', FabIcon)
   }
 }
 export default plugin
