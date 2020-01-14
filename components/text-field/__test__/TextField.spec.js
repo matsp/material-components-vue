28fd54c348ef1ef1e2326c311cd819f231a0638f
Vinay Pai <vinay@vinaypai.com>
2020-01-14 02:55:15 -0500

    fix: Restore broken fullwidth textarea with label (#523)
    Fullwidth textareas are allowed (upstream demo has an example) but
    wasn't working
    
    Write a console warning instead of silently ignoring invalid config
    options. The old behavior had potential to cause confusion.
    
    Add tests of text area and the new warnings.
    
    Fixes: #509

--- components/text-field/__test__/TextField.spec.js
+++ components/text-field/__test__/TextField.spec.js
@@ -2,6 +2,7 @@ import 'mutationobserver-shim'
 import { mount } from '@vue/test-utils'
 import TextField from '../TextField.vue'
 import TextFieldIcon from '../TextFieldIcon.vue'
+import FloatingLabel from '../../floating-label/FloatingLabel.vue'
 
 describe('Text Field', () => {
   it('should mount', () => {
@@ -68,6 +69,37 @@ describe('Text Field', () => {
     expect(wrapper.classes()).toContain('mdc-text-field--fullwidth')
   })
 
+  it('should warn of fullWidth text input with floating label', () => {
+    const warnfn = console.warn
+    console.warn = jest.fn()
+    mount(TextField, {
+      propsData: {
+        fullWidth: true
+      },
+      slots: {
+        default: '<m-floating-label>My Label</m-floating-label>'
+      },
+      stubs: {
+        'm-floating-label': FloatingLabel
+      }
+    })
+    expect(console.warn).toHaveBeenCalledWith(expect.stringContaining('floating label'))
+    console.warn = warnfn
+  })
+
+  it('should warn of fullWidth text input with outlined style', () => {
+    const warnfn = console.warn
+    console.warn = jest.fn()
+    mount(TextField, {
+      propsData: {
+        fullWidth: true,
+        outlined: true
+      }
+    })
+    expect(console.warn).toHaveBeenCalledWith(expect.stringContaining('outlined style'))
+    console.warn = warnfn
+  })
+
   it('should render and emit', () => {
     const wrapper = mount(TextField, {
       propsData: {
@@ -99,5 +131,36 @@ describe('Text Field', () => {
     })
     expect(wrapper).toMatchSnapshot()
     expect(wrapper.classes()).toContain('mdc-text-field--with-trailing-icon')
+  })
+
+  it('should render textarea', () => {
+    const wrapper = mount(TextField, {
+      propsData: {
+        textarea: true
+      }
+    })
+
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.classes()).toContain('mdc-text-field--textarea')
+  })
+
+  it('should render full-width textarea with outlined label', () => {
+    const wrapper = mount(TextField, {
+      propsData: {
+        textarea: true,
+        fullWidth: true
+      },
+      slots: {
+        default: '<m-floating-label>My Label</m-floating-label>'
+      },
+      stubs: {
+        'm-floating-label': FloatingLabel
+      }
+    })
+
+    expect(wrapper).toMatchSnapshot()
+    expect(wrapper.classes()).toContain('mdc-text-field--fullwidth')
+    expect(wrapper.classes()).toContain('mdc-text-field--textarea')
+    expect(wrapper.find('.mdc-notched-outline').exists()).toBe(true)
   })
 })
